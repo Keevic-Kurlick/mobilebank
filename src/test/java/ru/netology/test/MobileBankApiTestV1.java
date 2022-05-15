1e8381d46b4550e142778e4b9a0e6860aef58627
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-15 15:30:34 +0300

    config and code

---
+++ src/test/java/ru/netology/test/MobileBankApiTestV1.java
@@ -0,0 +1,28 @@
+package ru.netology.test;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+public class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts(){
+       //Given - when - then
+       given()
+               .baseUri("http://localhost:9999/api/v1")
+               .when()
+               .get("/demo/accounts")
+               .then()
+               .statusCode(200)
+               .header("Content-Type", "application/json; charset=UTF-8")
+               .contentType(ContentType.JSON)
+               .body("", hasSize(3))
+               .body("[1].currency", equalTo("USD"))
+               .body("[2].balance", greaterThan(0))
+               .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+               .body("every { it.balance >= 0}", is(true));
+;    }
+}
