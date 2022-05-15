71e837abb75ca6e7af04f8ea7adc6d29d83a8760
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-15 16:17:05 +0300

    check incorrect size in test

--- src/test/java/ru/netology/test/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/test/MobileBankApiTestV1.java
@@ -9,20 +9,20 @@ import static org.hamcrest.Matchers.*;
 
 public class MobileBankApiTestV1 {
     @Test
-    void shouldReturnDemoAccounts(){
-       //Given - when - then
-       given()
-               .baseUri("http://localhost:9999/api/v1")
-               .when()
-               .get("/demo/accounts")
-               .then()
-               .statusCode(200)
-               .header("Content-Type", "application/json; charset=UTF-8")
-               .contentType(ContentType.JSON)
-               .body("", hasSize(3))
-               .body("[1].currency", equalTo("USD"))
-               .body("[2].balance", greaterThan(0))
-               .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-               .body("every { it.balance >= 0}", is(true));
-;    }
+    void shouldReturnDemoAccounts() {
+        //Given - when - then
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .contentType(ContentType.JSON)
+                .body("", hasSize(5))
+                .body("[1].currency", equalTo("USD"))
+                .body("[2].balance", greaterThan(0))
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("every { it.balance >= 0}", is(true));
+    }
 }
